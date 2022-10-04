c5c85911706ef742f14287440e0602c3ceb06839
ArBe <argon-beryllium@protonmail.com>
2022-10-04 18:46:09 +0200

    fixed vecn copy constructor

--- vecn.h
+++ vecn.h
@@ -26,7 +26,7 @@ struct vecn
 		for(auto i = 0; i != N; i++) val[i] = v_[i];
 	}
 	template <uint8_t N2, typename T2>
-	vecn(const T2& vt_) : val(vt_.v), x(val[0]), y(N>1?val[1]:val[0]), z(N>2?val[2]:val[0]), w(N>3?val[3]:val[0]) {}
+	vecn(const vecn<N2, T2>& vt_) : val(vt_.v), x(val[0]), y(N>1?val[1]:val[0]), z(N>2?val[2]:val[0]), w(N>3?val[3]:val[0]) {}
 
 	T getLength() const { return std::sqrt(getLengthSquare()); }
 	T getLengthSquare() const
@@ -96,14 +96,14 @@ template <uint8_t N, typename T1, typename T2>
 inline vecn<N, T1> operator*(const vecn<N, T1>& v, const T2& s)
 {
 	vecn<N, T1> out(v);
-	for(auto& e : v.val) e *= s;
+	for(auto& e : out.val) e *= s;
 	return out;
 }
 template <uint8_t N, typename T1, typename T2>
 inline vecn<N, T1> operator/(const vecn<N, T1>& v, const T2& s)
 {
 	vecn<N, T1> out(v);
-	for(auto& e : v.val) e /= s;
+	for(auto& e : out.val) e /= s;
 	return out;
 }
 template <uint8_t N, typename T1, typename T2>
@@ -112,7 +112,7 @@ template <uint8_t N, typename T1, typename T2>
 inline vecn<N, T1> operator/(const T1& s, const vecn<N, T2>& v)
 {
 	vecn<N, T1> out(v);
-	for(auto& e : v.val) e = s/static_cast<T1>(e);
+	for(auto& e : out.val) e = s/static_cast<T1>(e);
 	return out;
 }
 
